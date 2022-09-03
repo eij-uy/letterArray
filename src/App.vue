1a99ea3bf65459d2d8b47cfbeab1097445924bf7
余杰 <yj2847659748@outlook.com>
2022-09-03 17:41:29 +0800

    开始

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+const arr: Array<any> = ["a", [1, 5]];
+</script>
+
+<template>
+  <div v-for="(item, index) in arr" :key="index" class="bigNote">
+    <HelloWorld :arr="item"></HelloWorld>
+  </div>
+</template>
+
+<style scoped>
+.bigNote {
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const props = defineProps({
+  arr: String || Array<any>,
+});
+</script>
+
+<template>
+  <div class="miniNote" v-for="(item, index) in arr" :key="index">
+    {{ item }}
+  </div>
+</template>
+
+<style scoped>
+.miniNote {
+  width: 30px;
+  height: 30px;
+}
+</style>
